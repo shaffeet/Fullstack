0338921d4a3f20bd84015a34cb7287f0b61ced99
Shavit Nahum <shavit.nahum@aalto.fi>
2023-02-04 18:03:57 +0200

    2.18 finished

--- osa2/maat/src/App.js
+++ osa2/maat/src/App.js
@@ -14,11 +14,19 @@ const Filter = ({ filter, onChange }) => {
 };
 
 const Country = ({ country }) => {
+  const languages = Object.values(country.languages);
   console.log(country);
   return (
     <div>
       <h1>{country.name.common}</h1>
-
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <h2>Languages:</h2>
+      <ul>
+        {languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
       <img
         src={country.flags.png}
         alt={country.flags.alt}
