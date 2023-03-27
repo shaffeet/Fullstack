dd6b0ae67d7e4a6fa7301bf010990eb916751701
Shavit Nahum <shavit.nahum@aalto.fi>
2023-03-27 17:24:16 +0300

    4.5 finished

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,7 +9,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  const favorite = blogs.reduce((previous, current) =>
+    previous.likes > current.likes ? previous : current
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
